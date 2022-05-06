ab6b0b37e953b319ed08da3c64f439302b760d09
N. Escobar <nickesc.media@gmail.com>
2022-05-06 12:35:13 -0700

    loweer running speed bc it runs into walls a lot and fix tiiming on rotate so it doesn't loop infintely,

--- states.py
+++ states.py
@@ -139,6 +139,7 @@ class _running():
                 _state.phase = _state.phases['running']
                 _state.startTime = currTime
                 #_state.endTime += currTime
+
                 _state.scaredyBot.drive()
 
             elif abs(_state.currAngle) < _state.goalAngle:
